e60130a1c2f4c0288b61a6eeb6e4bb84278eff2b
hector <nseng@codiz.co.uk>
2019-09-29 19:44:16 +0200

    new file:   SCRIPTS/create_database_BSBBANK_OLTP.sql 	new file:   gitflowstart.bat 	new file:   gitflowstop.bat

---
+++ gitflowstart.bat
@@ -0,0 +1,6 @@
+git flow release start 1.0
+
+git flow feature start STEP1
+git flow feature start STEP2
+git flow feature start STEP3
+git flow feature start STEP4
---
+++ gitflowstop.bat
@@ -0,0 +1,6 @@
+git flow feature stop STEP1
+git flow feature stop STEP2
+git flow feature stop STEP3
+git flow feature stop STEP4
+git flow release stop 1.0
+
