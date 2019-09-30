64b0ad5c1b0f5470179e21fe40185eb387322891
hector <nseng@codiz.co.uk>
2019-09-30 21:12:58 +0200

    Changes to be committed: 	deleted:    DATALOADS/ACCOUNTS/tbl_Ref_Account_Types.csv 	deleted:    DATALOADS/ACCOUNTS/tbl_Ref_Transaction_Types.csv 	new file:   DATALOADS/dataload_report.pdf 	new file:   DIAGRAMS/Accounts.html 	new file:   DIAGRAMS/BSBANK.html 	new file:   DIAGRAMS/Branches.html 	new file:   DIAGRAMS/Customers.html 	new file:   DIAGRAMS/DESIGN_SPEC.gif 	deleted:    SCRIPTS/hotfixes.sql

--- SCRIPTS/hotfixes.sql
+++
@@ -1,9 +0,0 @@
--- ISSUE1: Time inserts to tbl_Transactions.transaction_date_time throwing errors.
--- HOTFIX1. changing column type: timestamp to datetime:
-
-ALTER TABLE scm_Accounts.tbl_Transactions 
-DROP COLUMN transaction_date_time
-GO
-ALTER TABLE scm_Accounts.tbl_Transactions
-    ADD transaction_date_time datetime  NOT NULL 
-GO
