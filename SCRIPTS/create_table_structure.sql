95abe03d5ed68836efa31d67edcd775f4e3b57fa
hector <nseng@codiz.co.uk>
2019-09-30 05:41:54 +0200

    modified:   create_table_structure.sql

--- SCRIPTS/create_table_structure.sql
+++ SCRIPTS/create_table_structure.sql
@@ -1,22 +1,51 @@
--- Create new schema 'scm_Branches', 'scm_Customers' and 'scm_Accounts'.
--- Drop schema if it already exists.
-IF OBJECT_ID('scm_Branches', 'U') IS NOT NULL
+-- Drop schema 'scm_Branches' if it already exists.
+IF OBJECT_ID('scm_Branches.tbl_Banks', 'U') IS NOT NULL
+DROP TABLE scm_Branches.tbl_Banks
+GO
+IF OBJECT_ID('scm_Branches.tbl_Ref_Branch_Types', 'U') IS NOT NULL
+DROP TABLE scm_Branches.tbl_Ref_Branch_Types
+GO
+IF OBJECT_ID('scm_Branches.tbl_Branches', 'U') IS NOT NULL
+DROP TABLE scm_Branches.tbl_Branches
+GO
+IF OBJECT_ID('scm_Branches', 'U') IS NULL
 DROP SCHEMA scm_Branches
 GO
-IF OBJECT_ID('scm_Customers', 'U') IS NOT NULL
+
+-- Drop schema 'scm_Customers' if it already exists.
+IF OBJECT_ID('scm_Customers.tbl_Customers', 'U') IS NOT NULL
+DROP TABLE scm_Customers.tbl_Customers
+GO
+IF OBJECT_ID('scm_Customers.tbl_Addresses', 'U') IS NOT NULL
+DROP TABLE scm_Customers.tbl_Addresses
+GO
+IF OBJECT_ID('scm_Customers', 'U') IS NULL 
 DROP SCHEMA scm_Customers
 GO
-IF OBJECT_ID('scm_Accounts', 'U') IS NOT NULL
+
+-- Drop schema 'scm_Accounts' if it already exists.
+IF OBJECT_ID('scm_Accounts.tbl_Ref_Account_Status', 'U') IS NOT NULL
+DROP TABLE scm_Accounts.tbl_Ref_Account_Status
+GO
+IF OBJECT_ID('scm_Accounts.tbl_Ref_Account_Types', 'U') IS NOT NULL
+DROP TABLE scm_Accounts.tbl_Ref_Account_Types	
+GO
+IF OBJECT_ID('scm_Accounts.tbl_Ref_Transaction_Types', 'U') IS NOT NULL
+DROP TABLE scm_Accounts.tbl_Ref_Transaction_Types
+GO
+IF OBJECT_ID('scm_Accounts.tbl_Transactions', 'U') IS NOT NULL
+DROP TABLE scm_Accounts.tbl_Transactions
+GO
+IF OBJECT_ID('scm_Accounts.tbl_Accounts', 'U') IS NOT NULL
+DROP TABLE scm_Accounts.tbl_Accounts
+GO
+IF OBJECT_ID('scm_Accounts', 'U') IS NULL 
 DROP SCHEMA scm_Accounts
 GO
-CREATE SCHEMA scm_Branches
-GO
-CREATE SCHEMA scm_Customers;
-GO
-CREATE SCHEMA scm_Accounts;
+
 
  /*
- creating following schema / table structure:
+ creating the following schema / table structure:
 
 
                       |   (A) scm_Accounts           |  (B)    scm_Customers               |  (C) scm_Branches  
@@ -31,45 +60,31 @@ CREATE SCHEMA scm_Accounts;
 */
 
 
--- (A) scm_Accounts
- 
--- Create a new table in schema 'scm_Accounts'
--- Drop the table if it already exists
-IF OBJECT_ID('scm_Accounts.tbl_Ref_Account_Status', 'U') IS NOT NULL
-DROP TABLE scm_Accounts.tbl_Ref_Account_Status
-GO
-IF OBJECT_ID('scm_Accounts.tbl_Ref_Account_Types', 'U') IS NOT NULL
-DROP TABLE scm_Accounts.tbl_Ref_Account_Types
-GO
-IF OBJECT_ID('scm_Accounts.tbl_Ref_Transaction_Types', 'U') IS NOT NULL
-DROP TABLE scm_Accounts.tbl_Ref_Transaction_Types
-GO
-IF OBJECT_ID('scm_Accounts.tbl_Transactions', 'U') IS NOT NULL
-DROP TABLE scm_Accounts.tbl_Transactions
-GO
-IF OBJECT_ID('scm_Accounts.tbl_Accounts', 'U') IS NOT NULL
-DROP TABLE scm_Accounts.tbl_Accounts
-GO
+-- (A) scm_Accounts 
+-------------------------------------------------------------------------------------------
 
 -- 2NF:
-
+CREATE SCHEMA scm_Accounts
+GO
 CREATE TABLE scm_Accounts.tbl_Ref_Account_Status  
 (
-        account_status_code varchar(8) NOT NULL, -- primary key column,
+        account_status_code varchar(8) NOT NULL, -- (PK),
         account_status_description char(20),
 		CONSTRAINT Pk_tbl_Ref_Account_status_account_status_code PRIMARY KEY  ( account_status_code )
 );
 GO
+
+
 CREATE TABLE scm_Accounts.tbl_Ref_Account_Types 
 ( 
-	account_type_code varchar(8) NOT NULL, -- primary key column,
+	account_type_code varchar(8) NOT NULL, -- (PK)
 	account_type_description char(20)    ,
 	CONSTRAINT Pk_tbl_Ref_Account_Types_account_type_code PRIMARY KEY  ( account_type_code )
  );
  GO
  CREATE TABLE scm_Accounts.tbl_Ref_Transaction_Types 
 ( 
-	transaction_type_code varchar(8) NOT NULL, -- primary key column,
+	transaction_type_code varchar(8) NOT NULL, -- (PK),
 	transaction_type_description char(20)    ,
 	CONSTRAINT Pk_tbl_Ref_Transaction_Types_transaction_type_code PRIMARY KEY  ( transaction_type_code )
  );
@@ -78,40 +93,38 @@ CREATE TABLE scm_Accounts.tbl_Ref_Account_Types
 -- 3NF: 
 
 CREATE TABLE scm_Accounts.tbl_Accounts ( 
-	account_number       int NOT NULL   ,
-	account_status_code  varchar(8)    ,
-	account_type_code    varchar(8)    ,
-	customer_id          int    ,
+	account_number       int NOT NULL   , --(PK)
+	account_status_code  varchar(8) NOT NULL   ,-- (FK)
+	account_type_code    varchar(8) NOT NULL   ,-- (FK)
+	customer_id          int NOT NULL , --(FK)
 	current_balance      money    ,
 	other_details        char(250)    ,
 	CONSTRAINT Pk_tbl_Accounts_account_number PRIMARY KEY  ( account_number )
  );
+ GO
+
  CREATE TABLE scm_Accounts.tbl_Transactions ( 
-	transaction_id       bigint NOT NULL   ,
-	account_number       int    ,
-	merchant_id          int    ,
-	transaction_type_code varchar(8)    ,
+	transaction_id       bigint NOT NULL   , --(PK)
+	account_number       int NOT NULL   , -- (FK)
+	merchant_id          int NOT NULL   , -- (FK)
+	transaction_type_code varchar(8) NOT NULL   , -- (FK)
 	transaction_date_time timestamp    ,
 	transaction_amount   money    ,
 	other_details        char(250)    ,
 	CONSTRAINT Pk_tbl_Transactions_transaction_id PRIMARY KEY  ( transaction_id )
  );
+ GO
+
 
  -- (B) scm_Customers:
- -- Create a new table in schema 'scm_Customers'. Drop the table if it already exists
-
-IF OBJECT_ID('scm_Customers.tbl_Customers', 'U') IS NOT NULL
-DROP TABLE scm_Customers.tbl_Customers
+ -------------------------------------------------------------------------------------------
+CREATE SCHEMA scm_Customers
 GO
-IF OBJECT_ID('scm_Customers.tbl_Addresses', 'U') IS NOT NULL
-DROP TABLE scm_Customers.tbl_Addresses
-GO
-
--- Create the table in the specified schema
-CREATE TABLE scm_Customers.tbl_Addresses
+-- 2NF
+CREATE TABLE scm_Customers.tbl_Addresses 
 (
-        address_id int NOT NULL, -- primary key column
-        line1 nvarchar(50) ,
+        address_id int NOT NULL, -- (PK)
+        line1 nvarchar(50) , 
         line2 nvarchar(50) ,
         town_city nvarchar(50) ,
         zip_postcode nvarchar(10) ,
@@ -122,14 +135,49 @@ CREATE TABLE scm_Customers.tbl_Addresses
 
 );
 GO
+--3NF
 CREATE TABLE scm_Customers.tbl_Customers
 (
-        customer_id int NOT NULL , -- primary key column
-		address_id int ,
-		branch_id smallint ,
+        customer_id int NOT NULL , -- (PK)
+		address_id int NOT NULL, --(FK)
+		branch_id smallint NOT NULL, --(FK)
 		personal_details char(50) ,
 		contact_details char(50)
         CONSTRAINT Pk_tbl_Customers_customer_id PRIMARY KEY  ( customer_id )
 
+);
+GO
+
+-- (C) scm_Branches 
+-------------------------------------------------------------------------------------------
+--2NF
+
+CREATE SCHEMA scm_Branches
+GO
+
+CREATE TABLE scm_Branches.tbl_Banks
+(
+	bank_id smallint NOT NULL , -- (PK)
+	bank_details char(50) ,
+	CONSTRAINT Pk_tbl_Banks_bank_id PRIMARY KEY  ( bank_id )
+);
+GO
+CREATE TABLE scm_Branches.tbl_Ref_Branch_Types
+(
+	branch_type_code varchar(8) NOT NULL , -- (PK)
+	branch_type_description char(50) ,
+	CONSTRAINT Pk_tbl_Ref_Branch_branch_type_code PRIMARY KEY  ( branch_type_code )
+);
+GO
+
+--3NF
+CREATE TABLE scm_Branches.tbl_Branches
+(
+	branch_id  smallint NOT NULL, -- (PK)
+	address_id int NOT NULL,  -- (FK)
+	bank_id smallint NOT NULL , -- (FK)
+	branch_type_code varchar(8) NOT NULL , -- (FK)
+	branch_details char(50) ,
+	CONSTRAINT Pk_tbl_Branches_branch_id PRIMARY KEY  ( branch_id )
 );
 GO
